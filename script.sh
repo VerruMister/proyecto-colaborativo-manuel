9955171355ae3c523aee5338e93b91ca5ac8c9b5
Manuel <psnmanuel72@gmail.com>
2025-05-04 12:30:17 +0200

    Añade funcionalidad para mostrar contenido de un archivo

--- script.sh
+++ script.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-echo "¡Hola! Este es un script básico para aprender Git desde cero."
+echo "¡Hola! Este es un script para aprender Git desde cero."
+echo "1) Mostrar archivos en el directorio actual"
+echo "2) Mostrar fecha y hora"
+echo "3) Mostrar contenido de un archivo"
+echo "4) Salir"
+read -p "Selecciona una opción: " opcion
+
+case $opcion in
+    1) ls ;;
+    2) date ;;
+    3)	read -p "Introduce el nombre del archivo: " archivo
+        if [[ -f "$archivo" ]]; then
+            cat "$archivo"
+        else
+            echo "El archivo no existe."
+        fi
+        ;;
+    4) echo "Adiós"; exit ;;
+    *) echo "Opción no válida";;
+esac
