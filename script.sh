8ea051f41f80869bd63a2ece44823f0198f42a4e
Manuel <psnmanuel72@gmail.com>
2025-05-04 12:38:19 +0200

    Añade funcionalidad para calcular factorial

--- script.sh
+++ script.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo "¡Hola! Este es un script básico para aprender Git desde cero."
+echo "¡Hola! Este es un script para aprender Git desde cero."
+echo "1) Mostrar archivos en el directorio actual"
+echo "2) Mostrar fecha y hora"
+echo "3) Calcular el factorial de un número"
+echo "4) Salir"
+read -p "Selecciona una opción: " opcion
+
+case $opcion in
+    1) ls ;;
+    2) date ;;
+    3)
+        echo "Esta opción está en construcción."
+        ;;
+    4) echo "Adiós"; exit ;;
+    *) echo "Opción no válida";;
+esac
